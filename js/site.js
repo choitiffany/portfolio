598323313fa4fe8442436e953b7fc1092e98e0f8
Tiffany Choi <tchoi@ucdavis.edu>
2016-04-28 00:59:18 -0700

    bunch of changes

--- js/site.js
+++ js/site.js
@@ -10,4 +10,10 @@ $(function () {
         }
     );
 
+
+    $('section#caption').hide();
+console.log("hover over ");
+    $('.story').hover(function () {
+        $('section#caption').slideToggle(500);
+});
 }); //end
