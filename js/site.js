7c06f3d3b6d2f29cdab8c4d53285e7a8c4beaf89
Tiffany Choi <tchoi@ucdavis.edu>
2016-05-04 18:47:50 -0700

    more updates and hover

--- js/site.js
+++ js/site.js
@@ -1,19 +1,61 @@
-$(function () {
-    console.log('ready');
-
-    $('.slide').hover(
-        function () {
-            $(this).find('.caption').slideToggle(300);
-        },
-        function () {
-            $(this).find('.caption').slideToggle(300);
-        }
-    );
+$(document).ready(function() {
+  console.log("ready");
 
 
-    $('section#caption').hide();
-console.log("hover over ");
-    $('.story').hover(function () {
-        $('section#caption').slideToggle(500);
-});
+  
+
+   $('#storyspace').hover(function () {
+        console.log("hover over");
+        $(this).attr('src', 'images/ss_hover.png').animate({
+     fadeIn:450
+        })
+    }, function () { //second function
+        $(this).attr('src', 'images/ss2.jpg').animate({
+            swing:450
+        })
+    });
+
+   $('#infographics').hover(function () {
+        console.log("hover over");
+        $(this).attr('src', 'images/infographic_hover.png').animate({
+     swing:450
+        })
+    }, function () { //second function
+        $(this).attr('src', 'images/cindymockup.jpg').animate({
+            swing:450
+        })
+    });
+
+     $('#soothe').hover(function () {
+        console.log("hover over");
+        $(this).attr('src', 'images/soothe_hover.png').animate({
+     swing:450
+        })
+    }, function () { //second function
+        $(this).attr('src', 'images/soothe.jpg').animate({
+            swing:450
+        })
+    });
+     $('#ui').hover(function () {
+        console.log("hover over");
+        $(this).attr('src', 'images/ui_hover.png').animate({
+     swing:450
+        })
+    }, function () { //second function
+        $(this).attr('src', 'images/RakMockup.jpg').animate({
+            swing:450
+        })
+    });
+
+          $('#paint').hover(function () {
+        console.log("hover over");
+        $(this).attr('src', 'images/paint_hover.png').animate({
+     swing:450
+        })
+    }, function () { //second function
+        $(this).attr('src', 'images/tote.jpg').animate({
+            swing:450
+        })
+    });
+
 }); //end
